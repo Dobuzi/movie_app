a6610cff18995d685762b401b371426477624ed3
김종원 <dobuzi@gimjong-won-ui-MacBookPro.local>
2020-05-10 16:10:57 +0900

    Life cycle of React Component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,47 @@
 import React from 'react';
-import Proptypes from 'prop-types'
+// import Proptypes from 'prop-types'
 
-function Tesla({ name, image, price }) {
-  return (
-  <div>
-    <h2>{name} : ${price}</h2>
-    <img src={image} alt={name}/>
-  </div>
-  )
-}
+class App extends React.Component {
+  state = {
+    count: 0
+  };
 
-Tesla.propTypes = {
-  id: Proptypes.number,
-  name: Proptypes.string.isRequired,
-  image: Proptypes.string.isRequired,
-  price: Proptypes.number.isRequired
-};
+  add = () => {
+    this.setState(current => ({ count: current.count + 1}))
+  };
 
-const TESLA_API = [
-  {
-    id: 1,
-    name: "Tesla Model S",
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/tesla-model-s-1563301327.jpg?crop=0.663xw:0.795xh;0.0929xw,0.205xh&resize=2048:*",
-    price: 100_000
-  },
-  {
-    id: 2,
-    name: "Tesla Model X",
-    image: "https://www.tesla.com/sites/tesla/files/curatedmedia/performance-hero%402.jpg",
-    price: 120_000
-  },
-  {
-    id: 3,
-    name: "Tesla Model 3",
-    image: "https://www.tesla.com/sites/default/files/model3-new/hero/model-3-hero-desktop-v2.jpg",
-    price: 50_000
-  },
-  {
-    id: 4,
-    name: "Tesla Model Y",
-    image: "https://www.tesla.com/sites/default/files/modelsx-new/social/model-y-social.png",
-    price: 60_000
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }))
+  };
+
+  constructor(props) {
+    super(props);
+    console.log("constructor");
   }
-];
 
-function renderTesla(model) {
-  return <Tesla key={model.id} name={model.name} image={model.image} price={model.price} />
-}
+  componentDidUpdate() {
+    console.log("component did update")
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <h1>TESLA Line-up</h1>
-      {TESLA_API.map(renderTesla)}
-    </div>
-  );
-}
+  componentDidMount() {
+    console.log("component did mount");
+  };
+
+  componentWillUnmount() {
+    console.log("component will unmount")
+  }
+
+  
+  render() {
+    console.log("I'm rendering")
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  };
+};
 
 export default App;
